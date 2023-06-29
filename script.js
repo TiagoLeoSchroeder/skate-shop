ac8d1dcaa0233c4436b89bdcc261933ebaf3c9b5
Tiago Leonardo Schroeder <111138016+TiagoLeoSchroeder@users.noreply.github.com>
2023-06-29 17:58:01 -0300

    alteração

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const imgs = document.getElementById("image");
-const img =  document.querySelectorAll("#image img");
+const imgs = document.getElementById("img");
+const img =  document.querySelectorAll("#img img");
 
 let idx = 0;
 
 function carrossel(){
     idx++;
 
-    if(idx > img.lenght - 1){
+    if(idx > img.length - 1){
         idx = 0;
     }
 
     imgs.style.transform = `translateX(${-idx * 870}px)`;
 }
 
-setInterval(carrossel, 1800);
+setInterval(carrossel, 3500);
